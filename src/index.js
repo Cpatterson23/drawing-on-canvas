800e686e72220c5b44590d1fa32f4cfdcd494fbb
cpatterson23 <ceddypbody@gmail.com>
2022-04-27 20:46:28 -0500

    cntrl button for canvas

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@
 const canvas = document.getElementById("my-canvas");
 const ctx = canvas.getContext("2d");
 
+
+
+let ctrl = draw
+
 /**
  * 
  * @param {MouseEvent} event 
  */
 function draw(event) {
-   ctx.fillRect(event.offsetX,event.offsetY,5,5);
+    if (event.ctrlKey) {
+        ctx.fillRect(event.offsetX,event.offsetY,5,5);
+    }else {
+        
+    }
+
 }
 
 canvas.addEventListener("mousemove",draw);
